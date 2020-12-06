a38c3ec7daa7128f14c5016ab995de2ecf06f041
menziess <stefan_schenk@hotmail.com>
2020-12-07 00:16:19 +0100

    Cleanup setup.py.

--- setup.py
+++ setup.py
@@ -4,16 +4,15 @@ from setuptools import (
     find_namespace_packages, setup)
 
 if __name__ == '__main__':
-    package_name = os.getenv('PACKAGE_NAME', 'demo')
     setup(
-        name=package_name,
+        name='demo',
         version=os.getenv('PACKAGE_VERSION', '0.0.dev0'),
         author='Stefan Schenk',
         author_email='stefan_schenk@hotmail.com',
         description='Just a packaging example project.',
         package_dir={'': 'src'},
         packages=find_namespace_packages('src', include=[
-            f'{package_name}*'
+            'demo*'
         ]),
         install_requires=[],
         entry_points={
